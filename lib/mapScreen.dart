fff3d1574aaa8bfd338782753f5f74bd9e76f466
Benja303 <anugamer303@gmail.com>
2024-01-24 10:22:36 -0300

    Location

--- lib/mapScreen.dart
+++ lib/mapScreen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:geolocator/geolocator.dart';
-// import 'package:location/location.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -12,60 +11,59 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  // Location location = new Location();
-
   LatLng? currentPosition;
-
-  // Future<LocationData> getLocation() async {
-  //   bool _serviceEnabled;
-  //   PermissionStatus _permissionGranted;
-
-  //   _serviceEnabled = await location.serviceEnabled();
-  //   if (!_serviceEnabled) {
-  //     _serviceEnabled = await location.requestService();
-  //     if (!_serviceEnabled) {
-  //       return Future.error("error");
-  //     }
-  //   }
-
-  //   _permissionGranted = await location.hasPermission();
-  //   if (_permissionGranted == PermissionStatus.denied) {
-  //     _permissionGranted = await location.requestPermission();
-  //     if (_permissionGranted != PermissionStatus.granted) {
-  //       return Future.error("error");
-  //     }
-  //   }
-
-  //   return await location.getLocation();
-  // }
-
-  Future<Position> getPosition() async {
-    LocationPermission permission;
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error("error");
-      }
-    }
-    return await Geolocator.getCurrentPosition();
-  }
-
-  void getCurrentPosition() async {
-    Position position = await getPosition();
-    setState(() {
-      currentPosition = LatLng(position.latitude, position.longitude);
-    });
-    // LocationData xd = await getLocation();
-    print(position.latitude);
-    print(position.longitude);
-    // print(xd);
-  }
+  Location location = Location();
 
   @override
   void initState() {
-    getCurrentPosition();
     super.initState();
+    location.onLocationChanged.listen((LocationData xd) {
+      setState(() {
+        currentPosition = LatLng(xd.latitude!, xd.longitude!);
+      });
+    });
+    getCurrentPosition();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<LocationData> getLocation() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return Future.error("Error: Servicio de ubicación no habilitado.");
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return Future.error("Error: Permiso de ubicación denegado.");
+      }
+    }
+
+    return await location.getLocation();
+  }
+
+  void getCurrentPosition() async {
+    try {
+      LocationData xd = await getLocation();
+      setState(() {
+        currentPosition = LatLng(xd.latitude!, xd.longitude!);
+      });
+      print("Latitude: ${xd.latitude}");
+      print("Longitude: ${xd.longitude}");
+    } catch (e) {
+      print("Error al obtener la ubicación: $e");
+    }
   }
 
   @override
@@ -77,23 +75,23 @@ class _MapScreenState extends State<MapScreen> {
               options:
                   MapOptions(initialCenter: currentPosition!, initialZoom: 17),
               children: [
-                  TileLayer(
-                    urlTemplate:
-                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-                    // Plenty of other options available!
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                ),
+                MarkerLayer(markers: [
+                  Marker(
+                    point: currentPosition!,
+                    alignment: const Alignment(-0.5, -2),
+                    child: const Icon(
+                      Icons.person_pin_circle,
+                      color: Color.fromARGB(255, 1, 48, 255),
+                      size: 50,
+                    ),
                   ),
-                  MarkerLayer(markers: [
-                    Marker(
-                        point: currentPosition!,
-                        alignment: const Alignment(-0.5, -2),
-                        child: const Icon(
-                          Icons.person_pin_circle,
-                          color: Color.fromARGB(255, 1, 48, 255),
-                          size: 50,
-                        ))
-                  ])
                 ]),
+              ],
+            ),
     );
   }
 }
